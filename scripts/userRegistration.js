d74bbe7e07432e1fe4698c6ba8a6a62c0c97fbdb
Lucas141418 <elucas878@gmail.com>
2023-04-22 00:10:31 -0600

    A little change

--- scripts/userRegistration.js
+++ scripts/userRegistration.js
@@ -137,16 +137,4 @@ const recoveryPassword = async (pEmail) => {
 }
 
 
-const logBookUsers = async () => {
-
-    try{
-        const response = await fetch("http://localhost:3000/login");
-
-        const dataUsers  = await response.json();
-        console.log(dataUsers);
-        return dataUsers;
-    } catch(error){
-        console.error(error);
-    }
-}
 
