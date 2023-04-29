b3092bacabe7d4eb5af0fc0e521485c38d312a76
AuriDB <123429667+AuriDB@users.noreply.github.com>
2023-04-29 16:50:24 -0600

    Update usuarios.js
    con cambios para el backend

--- scripts/usuarios.js
+++ scripts/usuarios.js
@@ -1,61 +1,176 @@
+// documend is ready
 $(document).ready(function () {
+
+  const tableUsers = document.getElementById("table-users");
+  const tbody = tableUsers.getElementsByTagName("tbody")[0];
+  const loading = `<div id="loading">loading...</div>`;
   
-    function generateRandomDate(start, end) {
-      return new Date(
-        start.getTime() + Math.random() * (end.getTime() - start.getTime())
-      );
+  async function getUsers() {
+  
+    try {    
+      console.log("Llamando usuarios....");
+      // add loading message while fetching data
+      tbody.innerHTML = loading;
+  
+      const res = await fetch("http://localhost:3000/users/");
+      console.log(res);
+  
+      if (!res.ok) {
+        throw new Error("Network response was not ok");
+      }
+  
+      const data = await res.json();
+      console.log(data);
+  
+      // populate table tbody with data from api and remove loading message
+      tbody.innerHTML = "";
+      data.forEach((user) => {
+        const row = tbody.insertRow(-1);
+        row.innerHTML = `
+        <td>${user.cedula}</td>
+        <td>${user.nombre}</td>
+        <td>${user.primerApellido} ${user.segundoApellido}</td>
+        <td>${user.correo}</td>
+        <td>${user.telefono}</td>
+        <td>${user.fechaNacimiento}</td>
+        <td>${user.unidad}</td>
+        <td>${user.role}</td>
+        <td>${user.status}</td>
+        <td>
+          <a href="/SICA-project-main/DogKnots/editarUsuarios.html?id=${user._id}" class="btn btn-primary">Editar</a>
+        </td>
+        `;
+      });
+  
+      console.log("Usuarios cargados");
+  
+    } catch (error) {
+      console.log(error);
+      // remove loading message and show error message
+      tbody.innerHTML = `<tr><td colspan="10">Error: ${error.message}</td></tr>`;
+    }
+  }
+  
+  getUsers();
+
+  
+  // edit user
+  async function editUser(id) {
+    console.log("Editando usuario");
+  
+    try {
+      const res = await fetch(`http://localhost:3000/users/${id}`);
+      const data = await res.json();
+      //console.log(data);
+  
+  
+      // roles array to populate select 1 is admin, 2 is user
+      const roles = [1, 2];
+      const status = ["Activo", "Inactivo"];
+  
+    
+      // create a swal modal to edit user
+      const { value: formValues } = await Swal.fire({
+        title: "Editar usuario",
+        html:
+          `<form id="form-edit-user">
+          <input id="cedula" class="swal2-input" value="${data.cedula}" disabled placeholder="Cédula">
+          <input id="nombre" class="swal2-input" value="${data.nombre}" disabled placeholder="Nombre">
+          <input id="primerApellido" class="swal2-input" value="${data.primerApellido}" disabled placeholder="Primer apellido">
+          <input id="segundoApellido" class="swal2-input" value="${data.segundoApellido}" disabled placeholder="Segundo apellido">
+          <input id="telefono" class="swal2-input" value="${data.telefono}" disabled placeholder="Teléfono">
+          <input id="correo" class="swal2-input" value="${data.correo}" disabled placeholder="Correo electrónico">
+          <input id="fechaNacimiento" class="swal2-input" value="${data.fechaNacimiento}" disabled placeholder="Fecha de nacimiento">
+          <input id="unidad" class="swal2-input" value="${data.unidad}" required placeholder="Unidad">
+          <select id="role" class="swal2-input" required>
+            <option value="" disabled selected>Seleccione un rol</option>
+            ${roles.map((role) => {
+              if (role === data.role) {
+                return `<option value="${role}" selected>${role}</option><option value="${role}">${role}</option>`;
+              } else {
+                return `<option value="${role}">${role}</option>`;
+              }
+            })}
+          </select>
+          <select id="status" class="swal2-input" required>
+            <option value="" disabled selected>Seleccione un estado</option>
+            ${status.map((status) => {
+              if (status === data.status) {
+                return `<option value="${status}" selected>${status}</option>`;
+              } else {
+                return `<option value="${status}">${status}</option>`;
+              }
+            })}
+          </select>
+        </form>`,
+        focusConfirm: false,
+        preConfirm: () => {
+          return [
+            document.getElementById("cedula").value,
+            document.getElementById("nombre").value,
+            document.getElementById("primerApellido").value,
+            document.getElementById("segundoApellido").value,
+            document.getElementById("telefono").value,
+            document.getElementById("correo").value,
+            document.getElementById("fechaNacimiento").value,
+            document.getElementById("unidad").value,
+            document.getElementById("role").value,
+            document.getElementById("status").value,
+          ];
+        }
+      });
+  
+      if (formValues) {
+        // show loading message while updating user
+        tbody.innerHTML = loading;
+  
+        const res = await fetch(`http://localhost:3000/users/${id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            cedula: formValues[0],
+            nombre: formValues[1],
+            primerApellido: formValues[2],
+            segundoApellido: formValues[3],
+            telefono: formValues[4],
+            correo: formValues[5],
+            fechaNacimiento: formValues[6],
+            unidad: formValues[7],
+            role: formValues[8],
+            status: formValues[9],
+          }),
+        });
+  
+        const data = await res.json();
+        console.log(data);
+  
+        // get updated users
+        getUsers();
+  
+        // show success message
+        Swal.fire({
+          icon: "success",
+          title: "Usuario actualizado",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+  
+  
+    } catch (error) {
+      console.log(error);
     }
   
-    function generateRandomCedula() {
-      const max = 7777777;
-      const min = 1000000;
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
+  }
   
-    function generateRandomPhoneNumber() {
-      const max = 89999999;
-      const min = 80000000;
-      return Math.floor(Math.random() * (max - min + 1)) + min;
+  // add event listener to table when click on edit button use data-id attribute to get id
+  tableUsers.addEventListener("click", (e) => {
+    if (e.target.id === "editar") {
+      const id = e.target.getAttribute("data-id");
+      editUser(id);
     }
+  });
   
-    function generateRandomEmail(name, lastName) {
-      const domains = [
-        "gmail.com",
-        "hotmail.com",
-        "outlook.com",
-      ];
-      const randomDomain = domains[Math.floor(Math.random() * domains.length)];
-      return `${name.toLowerCase()}.${lastName.toLowerCase()}@${randomDomain}`;
-    }
-  
-    function generateRandomUnit() {
-      const units = ["Unidad 1", "Unidad 2", "Unidad 3", "Unidad 4"];
-      return units[Math.floor(Math.random() * units.length)];
-    }
-  
-    // popular tabla
-    const table = document.getElementById("table-users");
-    const tbody = table.getElementsByTagName("tbody")[0];
-    for (let i = 0; i < 10; i++) {
-      const row = tbody.insertRow();
-      const name = names[Math.floor(Math.random() * names.length)];
-      const lastName = lastNames[Math.floor(Math.random() * lastNames.length)];
-      const cedula = generateRandomCedula();
-      const email = generateRandomEmail(name, lastName);
-      const nacimiento = generateRandomDate(
-        new Date(1950, 0, 1),
-        new Date(2000, 0, 1)
-      );
-      const telefono = generateRandomPhoneNumber();
-      const unidad = generateRandomUnit();
-      row.innerHTML = `
-                  <td>${cedula}</td>
-                  <td>${name}</td>
-                  <td>${lastName}</td>
-                  <td>${email}</td>
-                  <td>${telefono}</td>
-                  <td>${nacimiento.toLocaleDateString()}</td>
-                  <td>${unidad}</td>
-              `;
-    }
   });
